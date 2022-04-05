262549d6a531a0750b6e7fa2f087972f9f961300
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-05 17:56:19 -0300

    Configuramos o Redux - em grupo através do LiveShare.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider>
+    <Provider store={ store }>
       <App />
     </Provider>
   </BrowserRouter>,
