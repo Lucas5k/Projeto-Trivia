2f9493e4ffd9873f22657be605f8610f841ba6ff
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-05 17:48:40 -0300

    Configuramos o Router - em grupo através do live share.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Ranking from './pages/Ranking';
+import Results from './pages/Results';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/game" component={ Game } />
+      <Route path="/game/ranking" component={ Ranking } />
+      <Route path="/game/results" component={ Results } />
+      <Route path="/game/settings" component={ Settings } />
+    </Switch>
   );
 }
