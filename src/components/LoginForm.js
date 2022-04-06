80b2966d79620fc79c826fd185927248b1e87bd3
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-06 16:39:25 -0300

    Req1: concluído. Fizemos em grupo usando live share do VSCode.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
+import './LoginForm.css';
 
 class LoginForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      playerName: '',
+      email: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleDisableButton = () => {
+    const { playerName, email } = this.state;
+    const emailRegExp = /^([a-z0-9]{1,}[._]{0,1}[a-z0-9]{1,})*(@[a-z0-9]{1,}.com)$/i;
+    if (playerName.length === 0 || !email.match(emailRegExp)) {
+      return true;
+    }
+    return false;
+  };
+
   render() {
+    const { playerName, email } = this.state;
+
     return (
-      <form>
-        Login
+      <form className="LoginForm">
+        <label htmlFor="input-player-name">
+          <input
+            id="input-player-name"
+            data-testid="input-player-name"
+            name="playerName"
+            type="text"
+            value={ playerName }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="input-gravatar-email">
+          <input
+            id="input-gravatar-email"
+            data-testid="input-gravatar-email"
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ this.handleDisableButton() }
+        >
+          Play
+        </button>
       </form>
     );
   }
---
+++ src/redux/actions/index.js
@@ -0,0 +1,1 @@
+// import { getToken, getQuestions, getAvatar } from '../../services';
