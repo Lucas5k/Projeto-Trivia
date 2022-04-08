83f2838cde598874c13d43167ae73822fa69f15d
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-04-08 17:51:11 -0300

    Main-group-23-req-15-17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import IndividualRanking from '../components/IndividualRanking';
 
 class Ranking extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <main>
+        <h1 data-testid="ranking-title">Ranking</h1>
         <ol>
           <IndividualRanking />
         </ol>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          Go Home
+        </button>
       </main>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
