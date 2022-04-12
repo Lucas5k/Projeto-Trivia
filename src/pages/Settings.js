5f242e6d034b2c2293e15ee8618fb852fb868bc7
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-04-11 23:58:48 -0300

    Colocamos uma mensagem de página em desenvolvimento na página Settings.

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Settings.css';
 
 class Settings extends Component {
   render() {
     return (
-      <main>
+      <main className="Settings">
         <h1 data-testid="settings-title">Settings</h1>
+        <p>This page is currently under development</p>
       </main>
     );
   }
